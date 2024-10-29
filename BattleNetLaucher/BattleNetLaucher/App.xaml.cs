d49784dbe75d581df1e5e5b59f4e50eb4dc98896
Nonok <nolanalizon1@gmail.com>
2024-10-29 09:10:24 +0100

    Init Project

---
+++ BattleNetLaucher/BattleNetLaucher/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BattleNetLaucher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
