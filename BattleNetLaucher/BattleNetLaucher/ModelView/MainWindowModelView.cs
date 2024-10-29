8f9b502e40ed88466fc7725af4de9593a4d6b360
Nonok <nolanalizon1@gmail.com>
2024-10-29 12:25:27 +0100

    fix

--- BattleNetLaucher/BattleNetLaucher/ModelView/MainWindowModelView.cs
+++ BattleNetLaucher/BattleNetLaucher/ModelView/MainWindowModelView.cs
@@ -11,7 +11,7 @@ namespace BattleNetLaucher.ModelView
 {
     public class MainWindowModelView : ModelViewBase
     {
-        List<Option> allOptions = new List<Option>();
+        public List<Option> AllOptions { get; set; } = new List<Option>();
         public RelayCommand HomeCommand => new RelayCommand(OptionsCallbacks.HomeButtonCallback);
         public RelayCommand GamesCommand => new RelayCommand(OptionsCallbacks.GamesButtonCallback);
         public RelayCommand ShopCommand => new RelayCommand(OptionsCallbacks.ShopButtonCallback);
@@ -32,13 +32,13 @@ namespace BattleNetLaucher.ModelView
 
             // Troisieme Categorie
             Option _guidedTour = new Option("Guided Tour",new RelayCommand(OptionsCallbacks.GuidedTourCallBack)); // can add an Icon as Third parameter (default as null)
-            allOptions.Add(_guidedTour);
+            AllOptions.Add(_guidedTour);
 
             // Quatrieme Categorie
             Option _disconnect = new Option("Disconnection", new RelayCommand(OptionsCallbacks.LeaveOrDisconnectCallBack)); // can add an Icon as Third parameter (default as null)
-            allOptions.Add(_disconnect);
+            AllOptions.Add(_disconnect);
             Option _leave = new Option("Leave", new RelayCommand(OptionsCallbacks.LeaveOrDisconnectCallBack)); // can add an Icon as Third parameter (default as null)
-            allOptions.Add(_leave);
+            AllOptions.Add(_leave);
 
         }
 
@@ -47,34 +47,34 @@ namespace BattleNetLaucher.ModelView
             //TO DO CREATE WINDOWS TO SHOW AND REPLACE "null" BY THE WANTED WINDOW
             //Premiere Categorie
             OpenWindowOption _parameters = new OpenWindowOption("parameters", new RelayCommand(OptionsCallbacks.WindowOpeningCallBack),null); // can add an Icon as Fourth parameter (default as null)
-            allOptions.Add(_parameters);
+            AllOptions.Add(_parameters);
 
             OpenWindowOption _bNetUpdates = new OpenWindowOption("Battle.net Updates", new RelayCommand(OptionsCallbacks.WindowOpeningCallBack),null); // can add an Icon as Fourth parameter (default as null)
-            allOptions.Add(_bNetUpdates);
+            AllOptions.Add(_bNetUpdates);
             //Troisieme Categorie
             OpenWindowOption _commentary = new OpenWindowOption("Send Commentary", new RelayCommand(OptionsCallbacks.WindowOpeningCallBack), null); // can add an Icon as Third parameter (default as null)
-            allOptions.Add(_commentary);
+            AllOptions.Add(_commentary);
             OpenWindowOption _bugReport = new OpenWindowOption("Report a Bug", new RelayCommand(OptionsCallbacks.WindowOpeningCallBack), null); // can add an Icon as Third parameter (default as null)
-            allOptions.Add(_bugReport);
+            AllOptions.Add(_bugReport);
         }
 
         void InitURLOptions()
         {
             URLOption _account = new URLOption("Account", null, AllURLs.GET_ACCOUNT_URL); // can add an Icon as Fourth parameter (default as null)
             _account.OptionCommand = new RelayCommand(OptionsCallbacks.URLCallBack);
-            allOptions.Add(_account);
+            AllOptions.Add(_account);
 
             URLOption _help = new URLOption("Help", null, AllURLs.GET_HELP_URL); // can add an Icon as Fourth parameter (default as null)
             _help.OptionCommand = new RelayCommand(OptionsCallbacks.URLCallBack);
-            allOptions.Add(_help);
+            AllOptions.Add(_help);
 
             URLOption _forums = new URLOption("Forums", null, AllURLs.GET_FORUMS_URL); // can add an Icon as Fourth parameter (default as null)
             _forums.OptionCommand = new RelayCommand(OptionsCallbacks.URLCallBack);
-            allOptions.Add(_forums);
+            AllOptions.Add(_forums);
 
             URLOption _mobileApp = new URLOption("Mobile App", null, AllURLs.GET_MOBILE_APP_URL); // can add an Icon as Fourth parameter (default as null)
             _mobileApp.OptionCommand = new RelayCommand(OptionsCallbacks.URLCallBack);
-            allOptions.Add(_mobileApp);
+            AllOptions.Add(_mobileApp);
         }
 
         void InitPermanentCommands()
